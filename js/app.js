7af5560b7e2579464e031fc6da9d4e33e549fa30
transformGit <2630243397@qq.com>
2016-08-17 17:20:30 +0800

    localStorage

--- js/app.js
+++ js/app.js
@@ -1,38 +1,36 @@
-(function(window){
-	window.angular.module('TodoApp',[]);
-	window.angular.module('TodoApp')
-		.controller('mainController',['$scope',function($scope){
-				$scope.text = '';
-				$scope.todoList = [{
-					text: 'AngularJS',
-					done: false
-				},
-				{
-					text: 'ReactJS',
-					done: false
-				},{
-					text: 'NodeJS',
-					done: false
-				}];
-				$scope.add=function(){
-					var text=$scope.text.trim();
-					if(text){
-						$scope.todoList.unshift({
-							text: text,
-							done: false
-						});
-						$scope.text = '';
-					}
-				}
-				$scope.delete=function(todo){
-						var index = $scope.todoList.indexOf(todo);
-						$scope.todoList.splice(index, 1);
-				}
-				$scope.doneCount=function(){
-					var temp = $scope.todoList.filter(function(item){
-						return item.done;
-					})
-					return temp.length;
-				}
-	}]);
+(function(window) {
+    window.angular.module('TodoApp', []);
+    window.angular.module('TodoApp')
+        .controller('mainController', ['$scope', function($scope) {
+            $scope.text = '';
+            $scope.todoList = [];
+            for (var i = 0, len = localStorage.length; i < len; i++) {
+                var key = localStorage.key(i);
+                var value = localStorage.getItem(key);
+                (function(val) {
+                    $scope.todoList.push({ text: val });
+                })(value)
+            }
+            $scope.add = function() {
+                var text = $scope.text.trim();
+                if (text) {
+                    $scope.todoList.unshift({
+                        text: text,
+                        done: false
+                    });
+                    localStorage.setItem(new Date().getTime(), text)
+                    $scope.text = '';
+                }
+            }
+            $scope.delete = function(todo) {
+                var index = $scope.todoList.indexOf(todo);
+                $scope.todoList.splice(index, 1);
+            }
+            $scope.doneCount = function() {
+                var temp = $scope.todoList.filter(function(item) {
+                    return item.done;
+                })
+                return temp.length;
+            }
+        }]);
 })(window)
